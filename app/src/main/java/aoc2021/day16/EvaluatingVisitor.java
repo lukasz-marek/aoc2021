da902c3026359facf0ae7ff441b8ff01bccf1de1
lmarek <marek.lucas@gmail.com>
2021-12-20 19:59:51 +0100

    day 16 - use bigInteger for computations

--- app/src/main/java/aoc2021/day16/EvaluatingVisitor.java
+++ app/src/main/java/aoc2021/day16/EvaluatingVisitor.java
@@ -1,17 +1,18 @@
 package aoc2021.day16;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-public class EvaluatingVisitor implements PacketVisitor<Long> {
+public class EvaluatingVisitor implements PacketVisitor<BigInteger> {
     @Override
-    public Long visit(LiteralPacket literalPacket) {
-        return literalPacket.getValue();
+    public BigInteger visit(LiteralPacket literalPacket) {
+        return BigInteger.valueOf(literalPacket.getValue());
     }
 
     @Override
-    public Long visit(OperatorPacket operatorPacket) {
+    public BigInteger visit(OperatorPacket operatorPacket) {
         switch (operatorPacket.getOperatorId()) {
             case 0:
                 return sum(operatorPacket.getContainedPackets());
@@ -32,40 +33,43 @@ public class EvaluatingVisitor implements PacketVisitor<Long> {
         }
     }
 
-    private long sum(List<Packet> packets) {
-        return packets.stream().map(packet -> packet.accept(this)).mapToLong(Long::longValue).sum();
-    }
-
-    private long product(List<Packet> packets) {
-        var result = 1;
+    private BigInteger sum(List<Packet> packets) {
+        var result = BigInteger.ZERO;
         for (var packet : packets)
-            result *= packet.accept(this);
+            result = result.add(packet.accept(this));
         return result;
     }
 
-    private long minimum(List<Packet> packets) {
+    private BigInteger product(List<Packet> packets) {
+        var result = BigInteger.ONE;
+        for (var packet : packets)
+            result = result.multiply(packet.accept(this));
+        return result;
+    }
+
+    private BigInteger minimum(List<Packet> packets) {
         return packets.stream().map(packet -> packet.accept(this)).min(Comparator.naturalOrder()).get();
     }
 
-    private long maximum(List<Packet> packets) {
+    private BigInteger maximum(List<Packet> packets) {
         return packets.stream().map(packet -> packet.accept(this)).max(Comparator.naturalOrder()).get();
     }
 
-    private long greaterThan(List<Packet> packets) {
+    private BigInteger greaterThan(List<Packet> packets) {
         var fist = packets.get(0).accept(this);
         var second = packets.get(1).accept(this);
-        return fist > second ? 1 : 0;
+        return fist.compareTo(second) > 0 ? BigInteger.ONE : BigInteger.ZERO;
     }
 
-    private long lessThan(List<Packet> packets) {
+    private BigInteger lessThan(List<Packet> packets) {
         var fist = packets.get(0).accept(this);
         var second = packets.get(1).accept(this);
-        return fist < second ? 1 : 0;
+        return fist.compareTo(second) < 0 ? BigInteger.ONE : BigInteger.ZERO;
     }
 
-    private long equalTo(List<Packet> packets) {
+    private BigInteger equalTo(List<Packet> packets) {
         var fist = packets.get(0).accept(this);
         var second = packets.get(1).accept(this);
-        return Objects.equals(fist, second) ? 1 : 0;
+        return Objects.equals(fist, second) ? BigInteger.ONE : BigInteger.ZERO;
     }
 }
