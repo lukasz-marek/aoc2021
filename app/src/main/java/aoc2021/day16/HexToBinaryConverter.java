6b55d5254a576ee311b30c952f7cd53d8c7a9d03
Łukasz Marek <marek.lucas@gmail.com>
2021-12-20 17:51:08 +0100

    day 16 - added 16to2 converter

---
+++ app/src/main/java/aoc2021/day16/HexToBinaryConverter.java
@@ -0,0 +1,33 @@
+package aoc2021.day16;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class HexToBinaryConverter {
+    private final static Map<Character, String> mapping = ImmutableMap.<Character, String>builder()
+            .put('0', "0000")
+            .put('1', "0001")
+            .put('2', "0010")
+            .put('3', "0011")
+            .put('4', "0100")
+            .put('5', "0101")
+            .put('6', "0110")
+            .put('7', "0111")
+            .put('8', "1000")
+            .put('9', "1001")
+            .put('A', "1010")
+            .put('B', "1011")
+            .put('C', "1100")
+            .put('D', "1101")
+            .put('E', "1110")
+            .put('D', "1111")
+            .build();
+
+    public String convertToBinary(String hex) {
+        var output = new StringBuilder();
+        for (var hexDigit : hex.toCharArray())
+            output.append(mapping.get(hexDigit));
+        return output.toString();
+    }
+}
