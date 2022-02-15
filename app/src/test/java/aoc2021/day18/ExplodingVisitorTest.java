8b358338270974e8d8edc821deb96146d181fb2f
lmarek <marek.lucas@gmail.com>
2022-02-15 23:16:04 +0100

    added extra tests

--- app/src/test/java/aoc2021/day18/ExplodingVisitorTest.java
+++ app/src/test/java/aoc2021/day18/ExplodingVisitorTest.java
@@ -37,7 +37,30 @@ class ExplodingVisitorTest {
 
     @Test
     public void explodesOnRight() {
-        throw new RuntimeException("");
+        // given
+        var node = Pair.of(Leaf.of(1), Pair.of(Leaf.of(2), Pair.of(Leaf.of(3), Pair.of(Leaf.of(4), Pair.of(4, 5)))));
+
+        // when
+        var result = node.accept(tested);
+
+        // then
+        Assertions.assertTrue(result);
+        var expected = Pair.of(Leaf.of(1), Pair.of(Leaf.of(2), Pair.of(Leaf.of(3), Pair.of(Leaf.of(8), Leaf.of(0)))));
+        Assertions.assertEquals(expected, node);
+    }
+
+    @Test
+    public void doesNothingWhenNestedTooShallowly() {
+        // given
+        var node = Pair.of(Leaf.of(1), Pair.of(Leaf.of(2), Pair.of(Leaf.of(3), Leaf.of(6))));
+
+        // when
+        var result = node.accept(tested);
+
+        // then
+        Assertions.assertFalse(result);
+        var expected = Pair.of(Leaf.of(1), Pair.of(Leaf.of(2), Pair.of(Leaf.of(3), Leaf.of(6))));
+        Assertions.assertEquals(expected, node);
     }
 
 }
